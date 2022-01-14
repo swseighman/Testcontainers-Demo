21270843511d9a6946297da2302daa0f373cc3f7
Scott <scottseighman@gmail.com>
2022-01-14 08:52:48 -0500

    Initial commit

--- src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
+++ src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
@@ -7,6 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class CustomerIntegrationTests {
@@ -14,8 +21,26 @@ public class CustomerIntegrationTests {
     @Autowired
     private CustomerDao customerDao;
 
+    private static final MySQLContainer container = (MySQLContainer) new MySQLContainer("mysql:latest")
+        .withReuse(true);
+
+    @BeforeAll
+    public static void setup() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+
     @Test
     void when_using_a_clean_db_this_should_be_empty() {
+
+        //Integer onYourMachine = container.getMappedPort(3306);
+
         List<Customer> customers = customerDao.findAll();
         assertThat(customers).hasSize(2);
     }
